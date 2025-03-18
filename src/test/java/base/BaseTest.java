9ae677f85df7c7b6d54a6c138d3e02d3496296d1
abutohan <allanbutohan00@gmail.com>
2025-03-19 02:33:42 +0800

    added A/B Testing page

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -5,10 +5,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import pages.HomePage;
 
 
 public class BaseTest {
 
+    protected HomePage homePage;
     private WebDriver driver;
 
     @BeforeClass
@@ -18,12 +20,14 @@ public class BaseTest {
 
     @BeforeMethod
     public void loadHomePage() {
+        System.out.println("Opening Browser");
         driver.get("https://the-internet.herokuapp.com/");
+        homePage = new HomePage(driver);
     }
 
     @AfterClass
     public void tearDown() {
+        System.out.println("Closing Browser");
         driver.quit();
     }
-
 }
