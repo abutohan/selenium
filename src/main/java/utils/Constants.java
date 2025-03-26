406620ab9030339698230b51002a3c9db43c1bf7
abutohan <allanbutohan00@gmail.com>
2025-03-27 01:51:40 +0800

    added Basic Auth page

--- src/main/java/utils/Constants.java
+++ src/main/java/utils/Constants.java
@@ -2,6 +2,7 @@ package utils;
 
 public class Constants {
 
+    public final static String BASE_URL = "https://the-internet.herokuapp.com";
     public final static int TIMEOUT = 30;
     public final static String CONFIG_PROPERTIES = "config.properties";
 }
