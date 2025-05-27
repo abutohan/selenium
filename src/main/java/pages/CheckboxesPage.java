83bb63a502e42faaeebde721392778b8b295fabb
abutohan <allanbutohan00@gmail.com>
2025-05-28 02:19:52 +0800

    added Checkboxes page

---
+++ src/main/java/pages/CheckboxesPage.java
@@ -0,0 +1,55 @@
+package pages;
+
+import base.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class CheckboxesPage extends BasePage {
+
+    @FindBy(tagName = "h3")
+    @CacheLookup
+    private WebElement headerTitle;
+
+    @FindBy(xpath = "//input[@type='checkbox']")
+    @CacheLookup
+    private List<WebElement> checkboxes;
+
+    public CheckboxesPage(WebDriver driver) {
+        super(driver);
+        PageFactory.initElements(driver, this);
+    }
+
+    public String getHeaderTitle() {
+        return getWait().until(ExpectedConditions.visibilityOf(headerTitle)).getText();
+    }
+
+    public void resetCheckboxes() {
+        for (WebElement checkbox : checkboxes) {
+            if (checkbox.isSelected()) checkbox.click();
+        }
+    }
+
+    public void tickCheckboxes(HashMap<Integer, Boolean> checkboxConfig) {
+        resetCheckboxes();
+        for (int i = 0; i < checkboxes.size(); i++) {
+            if (checkboxConfig.get(i)) {
+                checkboxes.get(i).click();
+            }
+        }
+    }
+
+    public List<WebElement> getCheckboxes() {
+        return getWait().until(ExpectedConditions.visibilityOfAllElements(checkboxes));
+    }
+
+    public int getCheckboxCount() {
+        return getWait().until(ExpectedConditions.visibilityOfAllElements(checkboxes)).size();
+    }
+}
