d70304030f57d2726c094a958bde9acbdc05de66
abutohan <allanbutohan00@gmail.com>
2025-06-13 00:56:30 +0800

    added Secure File Download page

---
+++ src/main/java/pages/SecureFileDownloadPage.java
@@ -0,0 +1,27 @@
+package pages;
+
+import base.BasePage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import static utils.Constants.BASE_URL;
+import static utils.SetupAuth.setupAuthentication;
+
+public class SecureFileDownloadPage extends BasePage {
+
+    private final By headerTitle = By.tagName("h3");
+
+    public SecureFileDownloadPage(WebDriver driver) {
+        super(driver);
+        PageFactory.initElements(driver, this);
+    }
+
+    public String getHeaderTitle(String username, String password) {
+        setupAuthentication(getDriver(), BASE_URL, username, password);
+        getDriver().get(String.format("%s/download_secure", BASE_URL));
+        return getWait().until(ExpectedConditions.visibilityOfElementLocated(headerTitle)).getText();
+    }
+
+}
