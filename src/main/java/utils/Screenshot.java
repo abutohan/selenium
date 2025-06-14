f8d269ef5fb046b9e10e39209e3bfe65720291bb
abutohan <allanbutohan00@gmail.com>
2025-06-15 02:43:27 +0800

    added screen shots to HTML report

--- src/main/java/utils/Screenshot.java
+++ src/main/java/utils/Screenshot.java
@@ -16,11 +16,19 @@ import static utils.ReadProperties.loadProperty;
 
 public class Screenshot {
 
-    public static String captureScreenshot(String screenshotName, WebDriver driver) throws IOException {
+    public static String captureScreenshot(String parentFolder, String testFolder, String screenshotName, WebDriver driver) throws IOException {
+
+        String screenshotPath = Paths.get(loadProperty().getProperty("screenshot-dir") + "\\" +parentFolder + "\\" + testFolder).toAbsolutePath().toString();
+        File screenShotDir = new File(screenshotPath);
+
+        if (!screenShotDir.exists()) {
+            screenShotDir.mkdirs();
+        }
+
+
         TakesScreenshot ts = (TakesScreenshot) driver;
         File source = ts.getScreenshotAs(OutputType.FILE);
-        String screenshotPath = Paths.get(loadProperty().getProperty("screenshot-dir")).toAbsolutePath().toString();
-        String destinationPath = screenshotPath + "\\" + screenshotName + "_" + new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date()) + ".png";
+        String destinationPath = screenshotPath + "\\" + screenshotName + "_" + new SimpleDateFormat("yyyyMMdd_HHmmsssss").format(new Date()) + ".png";
         Path destination = Paths.get(destinationPath);
         Files.copy(source.toPath(), destination);
         return destinationPath;
--- src/test/java/pages/statuscodes/StatusCodesTest.java
+++ src/test/java/pages/statuscodes/StatusCodesTest.java
@@ -2,6 +2,7 @@ package pages.statuscodes;
 
 import base.BaseTest;
 import org.json.JSONObject;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -17,6 +18,11 @@ import static utils.ReadJSON.getTestDataFromJSON;
 public class StatusCodesTest extends BaseTest {
 
     private StatusCodesPage statusCodesPage;
+
+    @BeforeClass
+    public void initTest() {
+        test = extent.createTest("Status Codes");
+    }
 
     @BeforeMethod
     public void initPage() {
